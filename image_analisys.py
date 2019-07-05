34f094409f544da71cca2068b382b0a731e7e778
DESKTOP-OCBONET\gabri <gabriel.moya@uib.es>
2019-07-05 13:28:04 +0200

    Funcional

--- image_analisys.py
+++ image_analisys.py
@@ -4,20 +4,21 @@ import pickle
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from features import glcm_F, HOG, pca_F
+from features import glcm_F
 
 
 s = 55
-d = 32.0
+d = 16.0
 
-clf = pickle.load(open("res_Random Forest_20190703-17.clf", "rb"))
+clf = pickle.load(open("res_Random Forest_20190704-17.clf", "rb"))
 
-img = cv2.imread(df.imatges + "classificacio_01.png", -1)
+img = cv2.imread(df.imatges + "prova_A.tif", -1)
 img = img[:, :, 0]
-resultat = img.copy()
+resultat = np.zeros(img.shape)
 
 img = img / d
 img = img.astype(np.uint8)
+resultat[:] = -1
 
 
 h, w, = img.shape
@@ -28,16 +29,12 @@ for i in range(s//2, h-(s//2), 1):
     for j in range(s//2, w-(s//2), 1):
 
         xs = []
-        submatrix = img[i:i + s, j:j + s]
+        submatrix = img[i-(s//2):i + (s//2), j-(s//2):j + (s//2)]
         glcm_features = glcm_F(submatrix, angles=df.angles, distances=df.dist, prop=df.prop, d=d)
 
-        features = np.zeros((glcm_features.shape[0]))  # + 9))
-        features[0: glcm_features.shape[0]] = glcm_features
-
-        tipus = clf.predict(features.reshape(1, -1))
+        tipus = clf.predict(glcm_features.reshape(1, -1))
 
         resultat[i, j] = tipus
-
 
 plt.imshow(resultat)
 plt.show()
