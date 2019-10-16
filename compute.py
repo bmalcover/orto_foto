5290645b1cc3cc2aee86b53b88a9152145424770
bmalcover <bilibiel @gmail.com>
2019-10-16 10:21:56 +0200

    no funcional, falla la integracio

--- compute.py
+++ compute.py
@@ -1,25 +1,30 @@
-import cv2
+import os
+import pickle
 from definitions import definitions as df
 from osgeo import gdal
-from osgeo import ogr
-from osgeo import gdal_array
+from image_analisys import calcul
 import numpy as np
-import matplotlib.pyplot as plt
-'''
-clf = pickle.load(open(df.clf + os.altsep + "res_Random Forest_20190909-19.clf", "rb"))
 
-'''
+print(df.clf)
+
+clf = pickle.load(open(df.clf + os.sep + "res_Random Forest_20190909-19.clf", "rb"))
+
+idx = 0
+xBSize = 1000
+yBSize = 1000
+
 out_driver = gdal.GetDriverByName("GTiff")
 gtif = gdal.Open(df.imatges + "Mosaic_orto56_STPH.tif")
+marjades = gdal.Open(df.imatges + "marjades_STPH.tif")
+mdp = gdal.Open(df.imatges + "Mosaic_MDP25_STPH_50cm.tif")
 
 x_offset, px_w, rot1, y_offset, rot2, px_h = gtif.GetGeoTransform()
 projection = gtif.GetProjection()
 band = gtif.GetRasterBand(1)
-rows, cols = band.YSize, band.XSize
-xBSize = 1000
-yBSize = 1000
+marges = marjades.GetRasterBand(1)
+alcades = mdp.GetRasterBand(1)
 
-idx = 0
+rows, cols = band.YSize, band.XSize  # Suposam que totes tenen el mateix size TODO comprovar
 
 for i in range(0, rows, yBSize):
 
@@ -36,11 +41,23 @@ for i in range(0, rows, yBSize):
         data = band.ReadAsArray(j, i, numCols, numRows)
         data = np.flip(data)  # ALERTA!
 
+        marge = marges.ReadAsArray(j, i, numCols, numRows)
+        marge = np.flip(marge)
+
+        altura = alcades.ReadAsArray(j, i, numCols, numRows)
+        altura = np.flip(altura)
+
         if np.unique(data).shape[0] > 1:  # si no tota la matriu és igual
+
+
+            #PROCESSAMENT
+
+            resultat = calcul(data, marge, altura, clf)
+
 
             outfile = "res/" + str(idx) + ".tiff"
             out_data = out_driver.Create(str(outfile), yBSize, xBSize, 1, gdal.GDT_UInt16)
-            out_data.GetRasterBand(1).WriteArray(data)
+            out_data.GetRasterBand(1).WriteArray(resultat)
 
             # this is how to get the coordinate in space.
             posX = (px_w * i) + (rot1 * j) + x_offset
@@ -49,9 +66,9 @@ for i in range(0, rows, yBSize):
             posX += px_w / 2.0
             posY += px_h / 2.0
 
-            out_data.SetGeoTransform([posY, px_w, rot1, posX, rot2,  px_h])
+            out_data.SetGeoTransform([posY, px_w, rot1, posX, rot2, px_h])
             out_data.SetProjection(projection)
             idx += 1
-
+            print(idx)
 
 print("pasisut")
